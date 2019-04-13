a6e42888d2577081aa56ef10fb86d9deb08f1688
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-04-12 21:37:25 -0300

    Remove spaces from the end of the lines

--- test/test_products.js
+++ test/test_products.js
@@ -6,15 +6,15 @@ var app = require('../app'),
   Product = require('../models/product');
 
 var expect = chai.expect;
-describe('API Tests', function() { 
+describe('API Tests', function() {
   var product = {
     "name": "OK",
     "price": 10
   };
-  describe('## Create product ', function() { 
-    it('should get a product', function(done) { 
-      request(app) .post('/products/create') .send(product) .end(function(err, res) { 
-        //expect(res.statusCode).to.equal(201); 
+  describe('## Create product ', function() {
+    it('should get a product', function(done) {
+      request(app) .post('/products/create') .send(product) .end(function(err, res) {
+        //expect(res.statusCode).to.equal(201);
         expect(1).to.equal(1);
         const product_query = Product.findById(res.body._id, function (err, object) {
           if (err) {
