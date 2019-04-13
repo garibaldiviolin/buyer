c6580347248009aab169cbf3f92142ff9a1a8dbd
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-04-12 21:31:26 -0300

    Remove spaces from the end of the lines

--- test/test_products.js
+++ test/test_products.js
@@ -12,7 +12,7 @@ describe('API Tests', function() {
     "price": 10
   };
   describe('## Create product ', function() { 
-    it('should create a product', function(done) { 
+    it('should get a product', function(done) { 
       request(app) .post('/products/create') .send(product) .end(function(err, res) { 
         //expect(res.statusCode).to.equal(201); 
         expect(1).to.equal(1);
@@ -26,7 +26,7 @@ describe('API Tests', function() {
           expect(product.price).to.equal(res.body.price);
           done();
         });
-      }); 
-    }); 
+      });
+    });
   });
 });
