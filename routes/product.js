e5410dc24ec183d7849152c5bab14f06af6dd7ff
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-04-12 22:13:29 -0300

    Remove test endpoint from controller

--- routes/product.js
+++ routes/product.js
@@ -5,10 +5,6 @@ var router = express.Router();
 var product_controller = require('../controllers/product');
 
 
-// a simple test url to check that all of our files are communicating correctly.
-router.get('/test', product_controller.test);
-
-
 router.post('/create', product_controller.product_create);
 
 router.get('/list', product_controller.product_list);
