cf3da27f183519ed0d852e21db29d6c3a6bd76c7
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-04-17 20:20:52 -0300

    Add jwtSecret and jetSession to config/development.js

--- config/development.js
+++ config/development.js
@@ -4,4 +4,6 @@ module.exports = {
   env: 'development',
   db: 'mongodb://localhost:27017/Tododb',
   port: process.env.PORT || 1234,
+  jwtSecret: "MyS3cr3tK3Y",
+  jwtSession: {session: false},
 };
