bb14694474663b570f2886e4f29da662997017b5
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-04-17 20:17:26 -0300

    Add token endpoint to routes/product.js

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,13 @@
+var bodyParser = require("body-parser");
+var jwt = require("jwt-simple");
+var auth = require("./auth.js")();
+var cfg = require("./config.js");
 var express = require('express');
 var router = express.Router();
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 var product_controller = require('../controllers/product');
+var user_controller = require("../controllers/users.js");
 
 
 router.post('/create', product_controller.product_create);
@@ -15,5 +20,6 @@ router.put('/:id/update', product_controller.product_update);
 
 router.delete('/:id/delete', product_controller.product_delete);
 
+router.post("/token", user_controller.generate_token);
 
 module.exports = router;
