62210978513039dfe9770a8200b08bbc955c0738
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-05-17 22:18:28 -0300

    Remove jwtSecret and jwtSession from development.js

--- config/development.js
+++ config/development.js
@@ -1,9 +1,7 @@
 'use strict';
 
 module.exports = {
-  env: 'development',
-  db: 'mongodb://localhost:27017/Tododb',
-  port: process.env.PORT || 1234,
-  jwtSecret: "MyS3cr3tK3Y",
-  jwtSession: {session: false},
+    env: 'development',
+    db: 'mongodb://localhost:27017/Tododb',
+    port: process.env.PORT || 1234,
 };
