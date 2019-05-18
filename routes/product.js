7ae33b85f97ac6a6730f0e722abc8955d8bf31a5
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-05-17 23:26:07 -0300

    Remove token endpoint and add authentication to product list endpoint

--- routes/product.js
+++ routes/product.js
@@ -1,25 +1,22 @@
 var bodyParser = require("body-parser");
 var jwt = require("jwt-simple");
-var auth = require("./auth.js")();
-var cfg = require("./config.js");
+var auth = require("../controllers/auth.js")();
+var cfg = require("../config/development.js");
 var express = require('express');
 var router = express.Router();
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 var product_controller = require('../controllers/product');
-var user_controller = require("../controllers/users.js");
 
 
 router.post('/create', product_controller.product_create);
 
-router.get('/list', product_controller.product_list);
+router.get('/list', auth.authenticate(), product_controller.product_list);
 
 router.get('/:id', product_controller.product_details);
 
 router.put('/:id/update', product_controller.product_update);
 
 router.delete('/:id/delete', product_controller.product_delete);
-
-router.post("/token", user_controller.generate_token);
 
 module.exports = router;
