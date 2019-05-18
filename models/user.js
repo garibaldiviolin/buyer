cc563c61d401c6f6b3cd0500df316ae5a2e4e8f7
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-05-18 17:25:56 -0300

    Fix script indentations

--- models/user.js
+++ models/user.js
@@ -23,22 +23,22 @@ UserSchema.methods.toJSON = function() {
 UserSchema.pre('save', function(next) {
     var user = this;
 
-// only hash the password if it has been modified (or is new)
-if (!user.isModified('password')) return next();
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
 
-// generate a salt
-bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
 
-// hash the password using our new salt
-bcrypt.hash(user.password, salt, function(err, hash) {
-    if (err) return next(err);
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
 
-// override the cleartext password with the hashed one
-user.password = hash;
-next();
-});
-});
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 UserSchema.methods.comparePassword = function(candidatePassword, cb) {
