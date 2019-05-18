fb23e05170b33304a058f1d6fb3dd25f85b18f9b
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-05-18 15:46:54 -0300

    Add user's routes

---
+++ routes/user.js
@@ -0,0 +1,21 @@
+var bodyParser = require("body-parser");
+var jwt = require("jwt-simple");
+var cfg = require("../config/development.js");
+var express = require('express');
+var router = express.Router();
+
+// Require the controllers WHICH WE DID NOT CREATE YET!!
+var user_controller = require('../controllers/user');
+
+
+router.post('/create', user_controller.user_create);
+
+router.get('/list', user_controller.user_list);
+
+router.get('/:id', user_controller.user_details);
+
+router.put('/:id/update', user_controller.user_update);
+
+router.delete('/:id/delete', user_controller.user_delete);
+
+module.exports = router;
