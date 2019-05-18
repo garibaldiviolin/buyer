6b11d105828c12ed5875f7b9d1bcdc48d862f63d
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-05-18 17:29:47 -0300

    Remove comments from script

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,6 @@ var cfg = require("../config/development.js");
 var express = require('express');
 var router = express.Router();
 
-// Require the controllers WHICH WE DID NOT CREATE YET!!
 var user_controller = require('../controllers/user');
 
 
