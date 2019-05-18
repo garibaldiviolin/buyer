f98556cd5612a006ddfa71b3d95b56738851d1fc
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-05-17 22:18:54 -0300

    Add jwtSecret and jwtSession to production.js

--- config/production.js
+++ config/production.js
@@ -1,7 +1,9 @@
 'use strict';
 
 module.exports = {
-  env: 'production',
-  db: process.env.MONGOHQ_URL || process.env.MONGODB_URI,
-  port: process.env.PORT || 1234,
+    env: 'production',
+    db: process.env.MONGOHQ_URL || process.env.MONGODB_URI,
+    port: process.env.PORT || 1234,
+    jwtSecret: "MyS3cr3tK3Y",
+    jwtSession: {session: false},
 };
