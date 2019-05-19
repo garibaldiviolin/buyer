9af8d80f3217bb5ed73585345032a9199b091748
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-05-19 20:44:43 -0300

    Fix database URL in test configuration

--- src/config/test.js
+++ src/config/test.js
@@ -4,15 +4,18 @@ var mongoose = require('mongoose');
 
 module.exports = {
   env: 'test',
-  db: 'mongodb://localhost:27017/TododbTest',
+  db: 'mongodb://localhost:27017/test',
   port: process.env.PORT || 1233,
 };
+
+console.log('configurou banco');
 
 //should we delete the database after the test run?
 var deleteAfterRun = false;
 
 //run once before all tests
 beforeEach(function (done) {
+    mongoose.connection.db.dropDatabase(done);
     done();
 });
 
