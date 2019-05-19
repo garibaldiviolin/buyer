10834b26be846e93fb580245454ad788c7900370
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-05-19 20:44:02 -0300

    Fix configuration import in app.js

--- src/app.js
+++ src/app.js
@@ -5,12 +5,13 @@ var product = require('./routes/product'); // Imports routes for the products
 var user = require('./routes/user'); // Imports routes for the users
 var auth = require('./routes/auth');
 var app = express();
+var config = require('./config/index');
 
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb://localhost:27017/productstutorial';
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var dev_db_url = 'mongodb://localhost:27017/buyer';
+var mongoDB = config.db || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
@@ -22,7 +23,7 @@ app.use('/products', product);
 app.use('/users', user);
 app.use('/auth', auth);
 
-var port = 1234;
+var port = config.port || 1234;
 
 app.listen(port, () => {
     console.log('Server is up and running on port number ' + port);
