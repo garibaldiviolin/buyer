a8779875d9d95db52e0e9c41edb6105f65544f3e
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-05-23 23:45:54 -0300

    Move dropDatabase and createJWTToken to config.test

--- src/config/test.js
+++ src/config/test.js
@@ -1,18 +1,53 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var User = require('../models/user');
+var app = require('../app');
+var chai = require('chai');
+var request = require('supertest');
 
-module.exports = {
-  env: 'test',
-  db: 'mongodb://localhost:27017/test',
-  port: process.env.PORT || 1233,
+var expect = chai.expect;
+
+async function dropDatabase() {
+    await mongoose.connection.db.dropDatabase();
 };
 
-beforeEach(function (done) {
-    mongoose.connection.db.dropDatabase(done);
-});
+async function createJWTToken(request) {
+
+    var user_json = {
+        _id: "5ce499d502b60729218eb6c2",
+        username: "test_user",
+        password: "test_password"
+    }
+
+    var user = new User(
+        user_json
+        );
+
+    await user
+    .save()
+    .then()
+    .catch(err => {
+        throw err;
+    });
+
+    var user_copy = JSON.parse(JSON.stringify(user_json));
+    delete user_copy._id;
+    const res = await request .post('/auth/token') .send(user_copy);
+    expect(res.statusCode).to.equal(200);
+    user_json.token = res.body.token;
+    return user_json;
+}
 
 //run once after all tests
 after(function (done) {
     mongoose.connection.db.dropDatabase(done);
 });
+
+module.exports = {
+  env: 'test',
+  db: 'mongodb://localhost:27017/test',
+  port: process.env.PORT || 1233,
+  createJWTToken: createJWTToken,
+  dropDatabase: dropDatabase
+};
