d302cb37436b828e1f6e3cb24d2d4e28a961a3d0
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-05-23 21:01:33 -0300

    Add user search during authentication (Passport Strategy)

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,6 @@
 var passport = require("passport");
 var passportJWT = require("passport-jwt");
-var users = require("../models/user.js");
+var User = require('../models/user');
 var cfg = require("../config/production.js");
 var ExtractJwt = passportJWT.ExtractJwt;
 var Strategy = passportJWT.Strategy;
@@ -11,21 +11,20 @@ var params = {
 
 module.exports = function() {
     var strategy = new Strategy(params, function(payload, done) {
-        if (!'id' in payload) {
-            return done(new Error("User not found"), null);
+        if (!('id' in payload)) {
+            return done(null, false, { message: 'Unauthorized' });
         }
 
         /*if (!'exp' in payload) {
             return done(new Error("Expiration not found"), null);
         }*/
 
-        return done(null, {id: payload._id});
-
-        if (user) {
-            return done(null, {id: user.id});
-        } else {
-            return done(new Error("User not found"), null);
-        }
+        User.findById(payload.id, function (err, user) {
+            if (err || !user) {
+                return done(null, false, { message: 'Unauthorized' });
+            }
+            return done(null, {id: user._id});
+        });
     });
     passport.use(strategy);
     return {
