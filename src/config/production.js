fb64fb60ba2e65792435d138c04ab5499b75fba4
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-05-26 20:23:13 -0300

    Change jwtSecret content

--- src/config/production.js
+++ src/config/production.js
@@ -4,6 +4,6 @@ module.exports = {
     env: 'production',
     db: process.env.MONGOHQ_URL || process.env.MONGODB_URI,
     port: process.env.PORT || 1234,
-    jwtSecret: "MyS3cr3tK3Y",
+    jwtSecret: "!Okok321",
     jwtSession: {session: false},
 };
