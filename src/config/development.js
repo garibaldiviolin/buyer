3bf459b0c7e211d34c31475b5b6a68aeeec2dfdc
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-05-26 20:22:31 -0300

    Add jwtSecret and jwtSession enviroment variables to development.js

--- src/config/development.js
+++ src/config/development.js
@@ -4,4 +4,6 @@ module.exports = {
     env: 'development',
     db: 'mongodb://localhost:27017/buyer-dev',
     port: process.env.PORT || 1234,
+    jwtSecret: "MyS3cr3tK3Y",
+    jwtSession: {session: false},
 };
